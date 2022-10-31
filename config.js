1d946fcaacd388b2abb3d45e6b2c491011dcefee
srjsurajydv <surajkumaryadav1001@gmail.com>
2022-10-31 17:14:35 +0530

    first commit

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-export const nftaddress = "0x4a568ddE248Db1245C5868ecC69EE55663bf1C93"
-export const nftmarketaddress = "0x5949c3Dca34846f6dde79Ae4E35279238cF86001"
+// export const nftaddress = "0x4a568ddE248Db1245C5868ecC69EE55663bf1C93"
+// export const nftmarketaddress = "0x5949c3Dca34846f6dde79Ae4E35279238cF86001"
+
+export const nftaddress = "0x119E315a25BfEC576Ae55f9255544411E38d1D24"
+
+export const nftmarketaddress = "0x928Ad6CA91105015fcd550C43bC4ECF87030DaB9"
